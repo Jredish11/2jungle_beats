a5f6d905795c3d2b3d1865415f42f5aa7e6f9053
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-03-23 21:24:51 -0600

    feat: Add methods append and count

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,5 +1,36 @@
 class LinkedList
+  attr_reader :head
   def initialize
-    
+    @head = nil
   end
+
+  def append(data)
+    if @head.nil?
+      head_node = Node.new(data)
+      @head = head_node
+    else
+      new_node = Node.new(data)
+      @head = current_node
+      until current_node.next_node.nil?
+        current_node = current_node.next_node
+      end
+      current_node.next_node = new_node
+    end
+  end
+
+  def count
+    count = 0
+   if @head.nil?
+    0
+   else
+    current_node = @head
+    until current_node.nil?
+      current_node = current_node.next_node
+      count += 1
+    end
+    count
+   end
+  end
+
+  
 end
