15c3f938da8ca6564be19c099921b330c775a88c
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-03-23 21:25:23 -0600

    test: Add tests for append and count

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -3,21 +3,40 @@ require "./lib/linked_list"
 require 'rspec'
 
 RSpec.describe LinkedList do
+  it 'exists' do
+    list = LinkedList.new
 
+    expect(list).to be_an_instance_of LinkedList
+  end
+
+  it 'has a head of the linkedlist' do
+    list = LinkedList.new
+
+    expect(list.head).to eq(nil)
+  end
+
+  it 'append data to list' do
+    list = LinkedList.new
+
+    list.append("doop")
+   
+    
+    expect(list.head.data).to eq("doop")
+    expect(list.head.next_node).to eq(nil)
+  end
+
+  it 'counts the data in list' do
+    list = LinkedList.new
+
+    list.append("doop")
+
+    expect(list.count).to eq(1)
+  end
 end
 
 
-# > list = LinkedList.new
-# => <LinkedList head=nil #45678904567>
-# > list.head
-# => nil
-# > list.append("doop")
-# => "doop"
-# > list
-# => <LinkedList head=<Node data="doop" next_node=nil #5678904567890> #45678904567>
-# > list.head.next_node
-# => nil
-# > list.count
-# => 1
+
+
+
 # > list.to_string
 # => "doop"
