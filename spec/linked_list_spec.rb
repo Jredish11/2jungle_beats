1117f633c4705203157faaa5b8229a4cdf9a99ee
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-03-24 15:25:04 -0600

    test: Add tests for count and to_string

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -29,14 +29,39 @@ RSpec.describe LinkedList do
     list = LinkedList.new
 
     list.append("doop")
-
     expect(list.count).to eq(1)
   end
+
+  
+  it 'generates a string of all the elements in the list' do
+    list = LinkedList.new
+
+    list.append("doop")
+
+    expect(list.to_string).to eq("doop")
+  end
+  
+  
+  it 'adds another node to list, can count nodes in list, return data as a string' do
+    list = LinkedList.new
+
+    expect(list.to_string).to eq("")
+
+    list.append("doop")
+    
+    list.append("deep")
+    
+    expect(list.head.next_node.data).to eq("deep")
+    
+    
+
+
+    expect(list.count).to eq(2)
+    expect(list.to_string).to eq("doop deep") 
+  end
+
+  
 end
 
 
 
-
-
-# > list.to_string
-# => "doop"
