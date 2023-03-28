f7c83ea0cd6cbd710ff0e8a94a77fe18927680f4
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-03-28 11:35:25 -0600

    test: Create spec for junglebeat class, write tests for initialize and append methods

---
+++ spec/jungle_beat_spec.rb
@@ -0,0 +1,40 @@
+require './lib/node'
+require './lib/linked_list'
+require './lib/jungle_beat'
+require 'rspec'
+
+RSpec.describe JungleBeat do
+  it 'exists' do
+    jb = JungleBeat.new
+
+    expect(jb).to be_an_instance_of JungleBeat
+  end
+  
+  it 'can create a linked list, checks if head of list is empty' do
+    jb = JungleBeat.new
+
+    jb.list 
+    
+    expect(jb.list.head).to eq(nil)
+  end
+  
+  it 'appends data to list, check if head.data is start of list' do
+    jb = JungleBeat.new
+    
+    jb.list 
+    jb.append("deep doo ditt")
+
+
+    expect(jb.list.head.data).to eq("deep")
+  end
+
+end
+
+
+
+# > jb.list.head.next_node.data
+# => "doo"
+# > jb.append("woo hoo shu")
+# => "woo hoo shu"
+# > jb.count
+# 6
